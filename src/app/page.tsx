dee3922ffd9cb74ba7d993526b4e30109d5d51f6
Ishwak Sharda <ishwak.sharda@gmail.com>
2024-11-02 12:03:43 -0700

    fix(votext): (main) implement vortex animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,36 @@
-import Canvas from './components/canvases';
-
+"use client";
+import Canvas from "./components/canvases";
+import { Vortex } from "../components/ui/vortex";
 export default function Home() {
-  return (
-    <div className='grid grid-cols-2'>
-        <Canvas algo='dfs'/>
+	return (
+		<div className="">
+			{/* <Canvas algo='dfs'/>
         <Canvas algo='bfs'/>
         <Canvas algo='astar'/>
-        <Canvas algo='ucs'/>
-    </div>
-  );
+        <Canvas algo='ucs'/> */}
+			<div className="w-screen h-screen mx-auto rounded-md overflow-hidden">
+				<Vortex
+					backgroundColor="black"
+					className="flex items-center flex-col justify-center px-2 md:px-10 py-4 w-full h-full"
+				>
+					<h2 className="text-white text-2xl md:text-6xl font-bold text-center">
+						Want to find a path?
+						<br />
+						Visualize it!
+					</h2>
+					<p className="text-white text-sm md:text-2xl max-w-xl mt-6 text-center">
+						Our interactive system allows you to visualize some of the most
+						famous path finding algorithms.
+					</p>
+					<div className="flex flex-col sm:flex-row items-center gap-4 mt-6">
+						<button className="px-4 py-2 bg-blue-600 hover:bg-blue-700 transition duration-200 rounded-lg text-white shadow-[0px_2px_0px_0px_#FFFFFF40_inset]">
+							Start app
+						</button>
+					</div>
+				</Vortex>
+			</div>
+		</div>
+	);
 }
+
+import React from "react";
